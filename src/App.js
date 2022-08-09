d212a920ad1f9a22bd2b15dab3eb2dfd1181f6e0
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-09 19:16:07 +0530

    basic redux store and reducer setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { createStore } from 'redux';
 import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
 import { cartItems } from './data';
+
+// initial state
+const initialState = {
+  count: 0,
+};
+
+// reducer
+function reducer(state, action) {
+  console.log({ state, action });
+}
+
+// store
+const store = createStore(reducer, initialState);
 
 function App() {
   return (
