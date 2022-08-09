a9dd8f5987dacf3baa0d62e85fa3c4f79805c580
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-09 17:31:43 +0530

    initial commit basic cart setup

---
+++ src/components/CartContainer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+const CartContainer = ({ cart = [] }) => {
+  if (cart.length === 0) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>Your Bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+  return (
+    <section className="cart">
+      <header>
+        <h2>Your Bag</h2>
+      </header>
+      <div>
+        {cart.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>$0.00</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn">clear cart</button>
+      </footer>
+    </section>
+  );
+};
+
+export default CartContainer;
