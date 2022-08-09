e62c35f4ad47f0b79f7e5fb441ea435892ced195
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-09 19:38:04 +0530

    dispatch action basic

--- src/App.js
+++ src/App.js
@@ -5,17 +5,33 @@ import { cartItems } from './data';
 
 // initial state
 const initialState = {
-  count: 73,
+  count: 0,
 };
 
 // reducer
 function reducer(state, action) {
   console.log({ state, action });
+  if (action.type === 'INCREASE') {
+    return { count: state.count + 1 };
+  }
+  if (action.type === 'DECREASE') {
+    return { count: state.count - 1 };
+  }
+  if (action.type === 'RESET') {
+    return { count: 0 };
+  }
   return state;
 }
 
 // store
 const store = createStore(reducer, initialState);
+store.dispatch({ type: 'DECREASE' });
+store.dispatch({ type: 'DECREASE' });
+store.dispatch({ type: 'RESET' });
+store.dispatch({ type: 'INCREASE' });
+store.dispatch({ type: 'INCREASE' });
+store.dispatch({ type: 'RANDOM' });
+store.dispatch({ type: 'INCREASE' });
 
 console.log(store.getState());
 
