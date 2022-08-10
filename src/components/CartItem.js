056d5600b5c6556457308255303bf81f8602beba
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-10 16:05:43 +0530

    decrease function implementation

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { INCREASE, REMOVE } from '../actions';
+import { DECREASE, INCREASE, REMOVE } from '../actions';
 
-const CartItem = ({ amount, img, price, title, remove, increase }) => {
+const CartItem = ({
+  amount,
+  img,
+  price,
+  title,
+  remove,
+  increase,
+  decrease,
+}) => {
   return (
     <article className="cart-item">
       <img src={img} alt={title} />
@@ -23,7 +31,7 @@ const CartItem = ({ amount, img, price, title, remove, increase }) => {
         {/* amount */}
         <p className="amount">{amount}</p>
         {/* decrease amount */}
-        <button className="amount-btn">
+        <button className="amount-btn" onClick={() => decrease()}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
           </svg>
@@ -34,7 +42,7 @@ const CartItem = ({ amount, img, price, title, remove, increase }) => {
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  const { id } = ownProps;
+  const { id, amount } = ownProps;
   return {
     remove: () =>
       dispatch({
@@ -45,6 +53,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
       }),
 
     increase: () => dispatch({ type: INCREASE, payload: { id } }),
+    decrease: () => dispatch({ type: DECREASE, payload: { id, amount } }),
   };
 };
 
