b3cfe7c6e1b0915d51716b756edd37eac5b35c25
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-10 13:28:42 +0530

    actions as action variables

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import { DECREASE, INCREASE } from './actions';
 import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
 import { cartItems } from './data';
@@ -12,31 +13,23 @@ const initialState = {
 // reducer
 function reducer(state, action) {
   console.log({ state, action });
-  if (action.type === 'INCREASE') {
+  if (action.type === INCREASE) {
     return { ...state, count: state.count + 1, name: 'Anna' };
   }
-  if (action.type === 'DECREASE') {
+  if (action.type === DECREASE) {
     return { ...state, count: state.count - 1 };
   }
-  if (action.type === 'RESET') {
-    return { ...state, count: 0 };
-  }
-  if (action.type === 'CHANGE_NAME') {
-    return { ...state, name: 'Brian' };
-  }
+
   return state;
 }
 
 // store
 const store = createStore(reducer, initialState);
-store.dispatch({ type: 'DECREASE' });
-store.dispatch({ type: 'DECREASE' });
-store.dispatch({ type: 'RESET' });
-store.dispatch({ type: 'INCREASE' });
-store.dispatch({ type: 'INCREASE' });
-store.dispatch({ type: 'RANDOM' });
-store.dispatch({ type: 'CHANGE_NAME' });
-store.dispatch({ type: 'INCREASE' });
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: INCREASE });
+store.dispatch({ type: INCREASE });
+store.dispatch({ type: INCREASE });
 
 console.log(store.getState());
 
