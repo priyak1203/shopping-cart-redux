b02e77f27c241173032fb7b44f7f9b70c27988d8
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-10 13:21:14 +0530

    state with more than one value

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import { cartItems } from './data';
 // initial state
 const initialState = {
   count: 0,
+  name: 'Peter',
 };
 
 // reducer
 function reducer(state, action) {
   console.log({ state, action });
   if (action.type === 'INCREASE') {
-    return { count: state.count + 1 };
+    return { ...state, count: state.count + 1, name: 'Anna' };
   }
   if (action.type === 'DECREASE') {
-    return { count: state.count - 1 };
+    return { ...state, count: state.count - 1 };
   }
   if (action.type === 'RESET') {
-    return { count: 0 };
+    return { ...state, count: 0 };
+  }
+  if (action.type === 'CHANGE_NAME') {
+    return { ...state, name: 'Brian' };
   }
   return state;
 }
@@ -31,6 +35,7 @@ store.dispatch({ type: 'RESET' });
 store.dispatch({ type: 'INCREASE' });
 store.dispatch({ type: 'INCREASE' });
 store.dispatch({ type: 'RANDOM' });
+store.dispatch({ type: 'CHANGE_NAME' });
 store.dispatch({ type: 'INCREASE' });
 
 console.log(store.getState());
